f9cadc19fa653962b385111d5d05488932e5b010
CarlosPast <cpastorgamero@safareyes.es>
2023-10-06 15:57:11 +0200

    primera subida, pagina principal reiniciada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
+    IndexComponent,
+    CabeceraComponent
   ],
   imports: [
     BrowserModule,
