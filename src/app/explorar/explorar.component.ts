d70465590626feaeda9b5c66003b6a805d799037
Eduardi <eruizgarcia@safareyes.es>
2023-11-09 16:09:06 +0100

    .

--- src/app/explorar/explorar.component.ts
+++ src/app/explorar/explorar.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import {ConectionService} from "../service/conection.service";
-import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-explorar',
@@ -8,14 +6,5 @@ import {Router} from "@angular/router";
   styleUrls: ['./explorar.component.css']
 })
 export class ExplorarComponent {
-
-  // eventos:any;
-  //
-  // constructor(private service:ConectionService, private router:Router) {
-  //
-  //   this.service.getEventos()
-  //     .subscribe(data => {this.eventos=data})
-  //
-  // }
 
 }
