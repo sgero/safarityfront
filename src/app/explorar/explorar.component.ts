c0376ace42927c56f202394d79fa39884f4ee9d0
CarlosPast <cpastorgamero@safareyes.es>
2023-11-09 16:17:02 +0100

    conexion con back listar en pruebas

--- src/app/explorar/explorar.component.ts
+++ src/app/explorar/explorar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {ConectionService} from "../service/conection.service";
 import {Router} from "@angular/router";
+import {OrganizacionService} from "../services/organizacion.service";
 
 @Component({
   selector: 'app-explorar',
@@ -10,6 +11,7 @@ import {Router} from "@angular/router";
 export class ExplorarComponent {
 
   eventos:any;
+  organizaciones : any;
 
   constructor(private service:ConectionService, private router:Router) {
 
@@ -17,5 +19,11 @@ export class ExplorarComponent {
       .subscribe(data => {this.eventos=data})
 
   }
+  // constructor(private  service:OrganizacionService, private router:Router) {
+  //   this.service.getOrganizacion().subscribe(data=> {this.organizaciones=data;})
+  // }
+
+
+
 
 }
