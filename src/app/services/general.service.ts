cb40848e1d8bfb8cdf4ab25d7302c43e316df3fb
SebasG <sgarciaguerrero@safareyes.es>
2023-11-14 20:45:21 +0100

    HOME PAGE + EVENTO

--- src/app/services/general.service.ts
+++ src/app/services/general.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import { Observable } from 'rxjs';
 import {Organizacion} from "../models/Organizacion";
 import {Evento} from "../models/Evento";
 
@@ -22,4 +23,12 @@ export class GeneralService {
   getEvento() {
     return this.http.get<Evento[]>(this.url+"/evento/listar");
   }
+
+  // private apiUrl = '/evento/listar'; //
+  //
+  // // constructor(private http: HttpClient) {}
+  // getEventos(): Observable<any[]> {
+  //   return this.http.get<any[]>(this.apiUrl);
+  // }
 }
+
