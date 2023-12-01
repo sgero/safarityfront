fb8aa48f867c269047575efcb23ac21ffdba64a4
Sebastian <sgarciaguerrero@safareyes.es>
2023-12-01 22:50:01 +0100

    componente usuario. prueba.

---
+++ src/app/usuario/usuario.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import {Rol} from "../models/Rol";
+
+@Component({
+  selector: 'app-usuario',
+  templateUrl: './usuario.component.html',
+  styleUrls: ['./usuario.component.css']
+})
+export class UsuarioComponent {
+}
