369129925e3cc8b35811bfdb18adcac9f9d41102
Eduardi <eruizgarcia@safareyes.es>
2023-12-12 20:09:49 +0100

    terminado modificar evento

--- src/app/modificarevento/modificarevento.component.ts
+++ src/app/modificarevento/modificarevento.component.ts
@@ -39,6 +39,18 @@ export class ModificareventoComponent implements OnInit {
 
   }
 
+  private formatearFecha(fecha: string): string {
+    const partes = fecha.split('/');
+    if (partes.length === 3) {
+      const [dia, mes, año] = partes;
+      // Formato ISO: YYYY-MM-DD
+      return `${año}-${mes.padStart(2, '0')}-${dia.padStart(2, '0')}`;
+    } else {
+      // Si el formato no es válido, devolver la fecha original
+      return fecha;
+    }
+  }
+
   formatDate() {
 
 
@@ -70,30 +82,96 @@ export class ModificareventoComponent implements OnInit {
 
   }
 
+  autocompletar(){
+
+    let check = (<HTMLInputElement>(document.getElementById("auto")))
+
+    if (check.checked) {
+
+      (<HTMLInputElement>(document.getElementById("nombre"))).value = this.eventomodificar.nombre;
+
+      (<HTMLInputElement>(document.getElementById("direccion"))).value = this.eventomodificar.direccion;
+
+      (<HTMLInputElement>(document.getElementById("descripcion"))).value = this.eventomodificar.descripcion;
+
+      (<HTMLInputElement>(document.getElementById("imagen"))).value = this.eventomodificar.imagen;
+
+      (<HTMLInputElement>(document.getElementById("precio"))).value = this.eventomodificar.precio;
+
+      (<HTMLInputElement>(document.getElementById("aforo"))).value = this.eventomodificar.aforo;
+
+      (<HTMLInputElement>(document.getElementById("fecha_lanzamiento"))).value = this.formatearFecha(this.eventomodificar.fecha_lanzamiento);
+
+      (<HTMLInputElement>(document.getElementById("fecha_venta"))).value = this.formatearFecha(this.eventomodificar.fecha_venta);
+
+      (<HTMLInputElement>(document.getElementById("fecha_inicio"))).value = this.formatearFecha(this.eventomodificar.fecha_inicio);
+
+      (<HTMLInputElement>(document.getElementById("fecha_fin"))).value = this.formatearFecha(this.eventomodificar.fecha_fin);
+
+      (<HTMLSelectElement>(document.getElementById("tipo_evento"))).value = this.eventomodificar.tipoEvento;
+
+      (<HTMLSelectElement>(document.getElementById("tipo_pago"))).value = this.eventomodificar.tipoPago;
+
+    }
+
+  }
   ngOnInit() {
     // Llama al servicio para obtener los datos del evento a modificar
     this.service.obtenerEventoPorId(this.eventoid).subscribe(data => {
 
-      this.eventomodificar= data;
-
-
+      this.eventomodificar = data;
+      this.eventomodificar.nombre;
     });
   }
 
   modificar() {
 
     this.evento.id = this.eventomodificar.id
-  if(this.evento.nombre == ''){this.evento.nombre= this.eventomodificar.nombre}
-  if(this.evento.descripcion == ''){this.evento.descripcion= this.eventomodificar.descripcion}
-  if(this.evento.direccion == ''){this.evento.direccion= this.eventomodificar.direccion}
-  if(this.evento.imagen == ''){this.evento.imagen= this.eventomodificar.imagen}
-  if(this.evento.aforo == 0){this.evento.aforo = this.eventomodificar.aforo}
-  if(this.evento.fecha_lanzamiento == 'NaN/NaN/NaN'){this.evento.fecha_lanzamiento = this.eventomodificar.fecha_lanzamiento}
-  if(this.evento.fecha_venta == 'NaN/NaN/NaN'){this.evento.fecha_venta = this.eventomodificar.fecha_venta}
-  if(this.evento.fecha_inicio == 'NaN/NaN/NaN'){this.evento.fecha_inicio = this.eventomodificar.fecha_inicio}
-  if(this.evento.fecha_fin == 'NaN/NaN/NaN'){this.evento.fecha_fin = this.eventomodificar.fecha_fin}
-  if(this.evento.tipoEvento == ''){this.evento.tipoEvento = this.eventomodificar.tipoEvento}
-  if(this.evento.tipoPago == ''){this.evento.tipoPago = this.eventomodificar.tipoPago}
+
+  if(this.evento.nombre == ''){
+    this.evento.nombre= this.eventomodificar.nombre
+  }
+
+  if(this.evento.descripcion == ''){
+    this.evento.descripcion= this.eventomodificar.descripcion
+  }
+
+  if(this.evento.direccion == ''){
+    this.evento.direccion= this.eventomodificar.direccion
+  }
+
+  if(this.evento.imagen == ''){
+    this.evento.imagen= this.eventomodificar.imagen
+  }
+
+  if(this.evento.aforo == 0){
+    this.evento.aforo = this.eventomodificar.aforo
+  }
+
+  if(this.evento.fecha_lanzamiento == 'NaN/NaN/NaN'){
+    this.evento.fecha_lanzamiento = this.eventomodificar.fecha_lanzamiento
+  }
+
+  if(this.evento.fecha_venta == 'NaN/NaN/NaN'){
+    this.evento.fecha_venta = this.eventomodificar.fecha_venta
+  }
+
+  if(this.evento.fecha_inicio == 'NaN/NaN/NaN'){
+    this.evento.fecha_inicio = this.eventomodificar.fecha_inicio
+  }
+
+  if(this.evento.fecha_fin == 'NaN/NaN/NaN'){
+    this.evento.fecha_fin = this.eventomodificar.fecha_fin
+  }
+
+  if(this.evento.tipoEvento == ''){
+    this.evento.tipoEvento = this.eventomodificar.tipoEvento
+  }
+
+  if(this.evento.tipoPago == ''){
+    this.evento.tipoPago = this.eventomodificar.tipoPago
+  }
+
   this.service.modificarEvento(this.evento).subscribe(data  =>{
     console.log(data);
     this.router.navigate(['/detallesEvento' , this.evento.id])
